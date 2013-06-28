fd9bdc285e8466adaebe41321872388f8ac24edd
Kamil Dziemianowicz <dziemian_rec@wp.pl>
2013-06-28 13:23:50 +0200

    Check if value is Array (key_value store problems truncating value to 1st character)

--- lib/i18n/backend/key_value.rb
+++ lib/i18n/backend/key_value.rb
@@ -90,7 +90,8 @@ module I18n
         def lookup(locale, key, scope = [], options = {})
           key   = normalize_flat_keys(locale, key, scope, options[:separator])
           value = @store["#{locale}.#{key}"]
-          value = ActiveSupport::JSON.decode(value)[0] if value
+          value = ActiveSupport::JSON.decode(value) if value
+          value = value[0] if value.is_a?(Array)
           value.is_a?(Hash) ? value.deep_symbolize_keys : value
         end
       end
